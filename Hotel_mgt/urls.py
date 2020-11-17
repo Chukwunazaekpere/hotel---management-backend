97597689cd9ed4e1fabfcfdd7c472b58b7eb8dff
Chukwunazaekpere <emmannuelchinaza5777@gmail.com>
2020-11-17 16:19:37 +0100

    recent commit

--- Hotel_mgt/urls.py
+++ Hotel_mgt/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', include('reception.urls')),
+    path('celestial/', include('reception.urls')),
 ]
